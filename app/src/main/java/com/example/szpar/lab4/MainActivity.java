98ad7899e3663fc0a68cc7139c22c87358d6a9dc
Jacek <szparags@gmail.com>
2016-04-22 12:59:16 +0200

    formularz

--- app/src/main/java/com/example/szpar/lab4/MainActivity.java
+++ app/src/main/java/com/example/szpar/lab4/MainActivity.java
@@ -48,7 +48,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nowyWpis(MenuItem mi) {
-        Intent intencja = new Intent(this, DodajWpis,class);
+        Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
